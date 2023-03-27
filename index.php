4a9ada2ac085b22367940405348f9eab665c657d
shehankminus <shehan.maverick@gmail.com>
2023-03-27 20:03:44 +0530

    sign btn

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
                 </div>
                 <butaton type="button" class="btn text-primary ms-3" data-bs-toggle="modal" data-bs-target="#searchModal"><i
                             class="fa fa-search"></i></butaton>
-                <a href="#" class="btn btn-primary py-2 px-4 ms-3 rounded-3">SignIn</a>
+                <a href="#" class="btn btn-primary py-2 px-4 ms-3" style="border-radius: 50px;">SignIn</a>
             </div>
         </nav>
     </div>
